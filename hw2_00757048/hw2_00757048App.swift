b8c0db59568a952dcc6690303d3044805b8101d9
aaaaaaaaaaa <aaaaa>
2020-10-20 17:19:34 +0800

    Initial Commit

---
+++ hw2_00757048/hw2_00757048App.swift
@@ -0,0 +1,17 @@
+//
+//  hw2_00757048App.swift
+//  hw2_00757048
+//
+//  Created by User09 on 2020/10/20.
+//
+
+import SwiftUI
+
+@main
+struct hw2_00757048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
